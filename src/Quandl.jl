10125481fe22a0f7b5c51d9239d57135d2f6251d
milktrader <milktrader@gmail.com>
2013-10-28 20:47:04 -0400

    still working on refactoring, big mess now

--- src/Quandl.jl
+++ src/Quandl.jl
@@ -1,8 +1,8 @@
-using DataFrames, Calendar
+using TimeSeries
 
 module Quandl
 
-using DataFrames, Calendar
+using TimeSeries
 
 import Base.show
 
@@ -29,34 +29,48 @@ function quandl(id::String, rows::Int, period::String)
     qdata = readlines(`curl -s "http://www.quandl.com/api/v1/datasets/$id.csv?sort_order=asc&rows=$rows&collapse=$period&auth_token=$auth_token"`)
   end
 
-  name_string = qdata[1]
-  val_string = qdata[2:end]
+  header = qdata[1]
+  vals   = qdata[2:end]
 
-# eliminate rows that have a date but no prices (market closed on a holiday)
-  va_valid = String[]
-  for i in 1:length(val_string)
-    #if length(val_string[i]) > 20 # FRED DGS10 is length 16
-    if length(val_string[i]) > 15
-    push!(va_valid, val_string[i])
-    end
-  end
+#### eliminate rows that have a date but no prices (market closed on a holiday)
+#### this shouldn't be necessary once readtable accepts http, and will be 
+#### better since NAs can be filled in. 
+###  va_valid = String[]
+###  for i in 1:length(val_string)
+###    #if length(val_string[i]) > 20 # FRED DGS10 is length 16
+###    if length(val_string[i]) > 15
+###    push!(va_valid, val_string[i])
+###    end
+###  end
 
-  #if length(val_string) < 1
-  if length(va_valid) < 1
-    error("you've probably exceeded your daily limit:\n 10 without token, 100 with a auth token")
-  end
+### need better catch
+###  #if length(val_string) < 1
+###  if length(va_valid) < 1
+###    error("you've probably exceeded your daily limit:\n 10 without token, 100 with a auth token")
+###  end
 
-  na  = split(name_string, ",")'
-  va  = split(va_valid[1], ",")'
+  h  = split(header, ",")'
+  v  = split(vals[1], ",")'
   
-  for i in 2:length(va_valid)         
-    va  = [va ; split(va_valid[i], ",")']
-  end
+###  for i in 2:length(va_valid)         
+###    va  = [va ; split(va_valid[i], ",")']
+###  end
 
-#  time_array = parse_date("yyyy-MM-dd", va[:,1]) # method not defined in master METADATA
-  time_array = Calendar.parse("yyyy-MM-dd", va[:,1])
+  vv = [split(vals[i], ",") for i=1:size(vals)[1]]
 
-  df = @DataFrame("Date" => time_array)
+####  time_array = parse_date("yyyy-MM-dd", va[:,1]) # method not defined in master METADATA
+###  time_array = Calendar.parse("yyyy-MM-dd", va[:,1])
+###
+###  df = @DataFrame("Date" => time_array)
+
+############## function readtime(filename::String) # Datetime version
+##############   df         = readtable(filename)
+##############   df["Date"] = Date[date(d) for d in df["Date"]]
+##############   df["Date"] = IndexedVector(df["Date"])
+##############   df[1,"Date"] > df[2, "Date"]?  flipud!(df): df
+##############   return df
+############## end
+  df = DataFrame("Date" = parse_date("yyyy-MM-dd", vv))
 
   for i in 2:length(na)
     colname  = na[i]
@@ -71,8 +85,8 @@ function quandl(id::String, rows::Int, period::String)
 #    end
 #  end
 
-   df["Date"] = IndexedVector(df["Date"]) # uncomment for Julia 0.2
-  df
+   df["Date"] = IndexedVector(df["Date"]) 
+  return df
 end
 
 #quandl(id) = quandl(id::String, 100000, "") # trouble passing too many rows into GOOG
