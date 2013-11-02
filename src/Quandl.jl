74c26fafb7191612d54701b32259a150e6df9aca
milktrader <milktrader@gmail.com>
2013-11-02 17:09:26 -0400

    moving to use Datetime, quandl api stripped to bare bones

--- src/Quandl.jl
+++ src/Quandl.jl
@@ -1,8 +1,8 @@
-using DataFrames, Calendar
+using TimeSeries
 
 module Quandl
 
-using DataFrames, Calendar
+using TimeSeries
 
 import Base.show
 
@@ -13,79 +13,21 @@ export quandl,
 ###### API ######################
 #################################
 
-function quandl(id::String, rows::Int, period::String)
+function quandl(id::String)
 
   auth_token = open(readall, Pkg.dir("Quandl/src/token/auth_token.jl")) 
- 
-#   period=="d"?period="daily":
-#   period=="w"?period="weekly":
-#   period=="m"?period="monthly":
-#   period=="q"?period="quarterly":
-#   period=="a"?period="annual":period
 
   if length(auth_token) >  50
-    qdata = readlines(`curl -s "http://www.quandl.com/api/v1/datasets/$id.csv?sort_order=asc&rows=$rows&collapse=$period"`)
+    qdata = download( "http://www.quandl.com/api/v1/datasets/$id.csv?sort_order=asc&rows=$rows&collapse=$period", "q.csv")
   else
-    qdata = readlines(`curl -s "http://www.quandl.com/api/v1/datasets/$id.csv?sort_order=asc&rows=$rows&collapse=$period&auth_token=$auth_token"`)
+    qdata = download("http://www.quandl.com/api/v1/datasets/$id.csv?sort_order=asc&rows=$rows&collapse=$period&auth_token=$auth_token", "q.csv")
   end
 
-  name_string = qdata[1]
-  val_string = qdata[2:end]
-
-# eliminate rows that have a date but no prices (market closed on a holiday)
-  va_valid = String[]
-  for i in 1:length(val_string)
-    #if length(val_string[i]) > 20 # FRED DGS10 is length 16
-    if length(val_string[i]) > 15
-    push!(va_valid, val_string[i])
-    end
-  end
-
-  #if length(val_string) < 1
-  if length(va_valid) < 1
-    error("you've probably exceeded your daily limit:\n 10 without token, 100 with a auth token")
-  end
-
-  na  = split(name_string, ",")'
-  va  = split(va_valid[1], ",")'
+  qframe = readtime("q.csv")
+  run(`rm q.csv`)
   
-  for i in 2:length(va_valid)         
-    va  = [va ; split(va_valid[i], ",")']
-  end
-
-#  time_array = parse_date("yyyy-MM-dd", va[:,1]) # method not defined in master METADATA
-  time_array = Calendar.parse("yyyy-MM-dd", va[:,1])
-
-  df = @DataFrame("Date" => time_array)
-
-  for i in 2:length(na)
-    colname  = na[i]
-    within!(df, :($colname = float($va[:,$i])))
-  end
-
-#  # correct the order if the DataFrame has more than 1 row 
-#  #if ~isempty(df[""][2]
-#  if nrow(df) > 1
-#    if df["Date"][1] > df["Date"][2]
-#      flipud!(df)
-#    end
-#  end
-
-   df["Date"] = IndexedVector(df["Date"]) # uncomment for Julia 0.2
-  df
+  return qframe
 end
-
-#quandl(id) = quandl(id::String, 100000, "") # trouble passing too many rows into GOOG
-
-#################################
-###### show #####################
-#################################
-
-function show(io::IO, t::CalendarTime)
-  s = format("yyyy-MM-dd", t)
-  print(io, s)
-end
-
 #################################
 ###### include ##################
 ##################################
