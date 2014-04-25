37309d8e42dd11ca8aa5742c56e94f031b2a4e24
milktrader <milktrader@gmail.com>
2014-04-24 23:18:25 -0400

    preparing nuke of dot julia

--- src/Quandl.jl
+++ src/Quandl.jl
@@ -13,9 +13,9 @@ function quandl(id::String; rows=100, period="daily")
   auth_token = open(readall, Pkg.dir("Quandl/src/token/auth_token.jl")) 
 
   if length(auth_token) >  50
-    qdata = download( "http://www.quandl.com/api/v1/datasets/$id.csv?sort_order=asc&rows=$rows&collapse=$period", "q.csv")
+    qdata = download( "http://www.quandl.com/api/v1/datasets/$id.csv?sort_order=dec&rows=$rows&collapse=$period", "q.csv")
   else
-    qdata = download("http://www.quandl.com/api/v1/datasets/$id.csv?sort_order=asc&rows=$rows&collapse=$period&auth_token=$auth_token", "q.csv")
+    qdata = download("http://www.quandl.com/api/v1/datasets/$id.csv?sort_order=dec&rows=$rows&collapse=$period&auth_token=$auth_token", "q.csv")
   end
 
   qarray = readtimearray("q.csv")
