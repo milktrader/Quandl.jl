9be4b870dcf0852a517d8ab8ddd4cbf68d7953cb
milktrader <milktrader@gmail.com>
2014-05-24 12:40:50 -0400

    changed format arg from DataType to string, changed messages for alternative data structures

--- src/api.jl
+++ src/api.jl
@@ -1,4 +1,4 @@
-function quandl(id::String; rows=100, period="daily", format=TimeArray)
+function quandl(id::String; rows=100, period="daily", format="TimeArray")
 
     auth_token = open(readall, Pkg.dir("Quandl/src/token/auth_token.jl")) 
   
@@ -8,14 +8,14 @@ function quandl(id::String; rows=100, period="daily", format=TimeArray)
         rq = get("http://www.quandl.com/api/v1/datasets/$id.csv?sort_order=des&rows=$rows&collapse=$period&auth_token=$auth_token")
     end
   
-    if format == TimeArray
+    if format == "TimeArray"
         timearray(rq) 
-    elseif format == DataFrame 
-        msg("DataFrame format not supported yet - please submit a PR \n 
-             You can lift the timearray code and create a file named dataframe.jl \n 
-             And then make replace this msg with dataframe(rq)") 
+    elseif format == "DataFrame" 
+        print_with_color(:yellow, "DataFrame format not supported yet - please submit a PR") 
+        println("")
     else
-        msg("wat? If you want this format, please submit a PR. :-)")
+        print_with_color(:yellow, "wat? If you want this format, please submit a PR. :-)")
+        println("")
     end
  
 end
