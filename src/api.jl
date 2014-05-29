859a2cdf7e969a737d67829476e05d9e8b464d2f
brk0_0 <brk0_0@server.fake>
2014-05-29 00:38:56 -0300

    api.jl: added query dict + cleanup

--- src/api.jl
+++ src/api.jl
@@ -1,20 +1,20 @@
 function quandl(id::String; order="des", rows=100, period="daily", format="TimeArray")
+    # Create a dictionary with the Query arguments that we pass to get() function
+    query_args = {"sort_order" => order, "rows" => rows, "collapse" => period}
 
+    # Open the auth_token file and add the token (if any) to the Query dictionary
     auth_token = open(readall, Pkg.dir("Quandl/src/token/auth_token.jl")) 
-  
-    if length(auth_token) >  50 
-        rq = get( "http://www.quandl.com/api/v1/datasets/$id.csv?sort_order=$order&rows=$rows&collapse=$period") 
-    else
-        rq = get("http://www.quandl.com/api/v1/datasets/$id.csv?sort_order=$order&rows=$rows&collapse=$period&auth_token=$auth_token")
-    end
-  
+    length(auth_token) >  50 ? query_args["auth_token"] = auth_token : nothing
+
+    # Get the response from Quandl's API, using Query arguments (see Response.jl README)
+    response = get("http://www.quandl.com/api/v1/datasets/$id.csv", query = query_args) 
+    
+    # Convert the response to the right DataType
     if format == "TimeArray"
-        timearray(rq) 
+        timearray(response) 
     elseif format == "DataFrame" 
-        dataframe(rq)
+        dataframe(response)
     else
-        print_with_color(:yellow, "wat? If you want this format, please submit a PR. :-)")
-        println("")
+        error("Invalid $format format. If you want this format implemented, please report an issue or submit a pull request.")
     end
- 
 end
