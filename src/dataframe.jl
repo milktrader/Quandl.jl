331338487aecfa0d2b4f6e68bf15577d3c5fe51c
Lucas Silva Simões <simoes.lucas.silva@gmail.com>
2014-05-30 23:12:36 -0300

    Indentation and TAB changes

--- src/dataframe.jl
+++ src/dataframe.jl
@@ -1,20 +1,20 @@
 function dataframe(response::Requests.Response)
 
-	buffer = PipeBuffer() # open a buffer in which to dump data
-	df     = DataFrame()  # init empty DataFrame
+    buffer = PipeBuffer() # open a buffer in which to dump data
+    df     = DataFrame()  # init empty DataFrame
 
-	try
-		# Write the data to the buffer
-	    write(buffer, response.data)
+    try
+        # Write the data to the buffer
+        write(buffer, response.data)
 
-		# Use DataFrame's readtable to read the data directly from buffer
-		df = readtable(buffer)
-		
-		# Convert dates to DateTime object
-		df[:Date] = Date{ISOCalendar}[date(d) for d in df[:Date]]
-	finally
-		close(buffer)	
-	end
+        # Use DataFrame's readtable to read the data directly from buffer
+        df = readtable(buffer)
+        
+        # Convert dates to DateTime object
+        df[:Date] = Date{ISOCalendar}[date(d) for d in df[:Date]]
+    finally
+        close(buffer)   
+    end
 
-	return df
+    return df
 end
--- src/timearray.jl
+++ src/timearray.jl
@@ -1,30 +1,30 @@
 function timearray(response::Requests.Response)
-	#This function transform the Response object on a TimeArray
+    #This function transform the Response object on a TimeArray
 
-	# Split the data on every "\n"
-	data = split(response.data, "\n")
+    # Split the data on every "\n"
+    data = split(response.data, "\n")
 
-	# Extract the head and body of the data
-	head = data[1]	
-	body = data[2:end]
+    # Extract the head and body of the data
+    head = data[1]  
+    body = data[2:end]
 
-	# Parse body	
-	body[end] == "" ? pop!(body) : nothing # remove trailing empty string if it's there
-	body      = [split(line, ",") for line in body] # split on comma
+    # Parse body    
+    body[end] == "" ? pop!(body) : nothing # remove trailing empty string if it's there
+    body      = [split(line, ",") for line in body] # split on comma
 
-	######### Timestamp
-	# take the first row (assuming it's date)
-	# TODO: regex query needed to catch edge cases
-	dates     = [line[1] for line in body]
-	timestamp = Date{ISOCalendar}[date(d) for d in dates] # parse dates
+    ######### Timestamp
+    # take the first row (assuming it's date)
+    # TODO: regex query needed to catch edge cases
+    dates     = [line[1] for line in body]
+    timestamp = Date{ISOCalendar}[date(d) for d in dates] # parse dates
 
-	######### Values 
-	vals = [line[2:end] for line in body] # get rows 2 to the end
-	vals = ss2float(vals) # SubString{ASCIIString}["88.94","89.48","88.8","89.23","30617089.0"]
+    ######### Values 
+    vals = [line[2:end] for line in body] # get rows 2 to the end
+    vals = ss2float(vals) # SubString{ASCIIString}["88.94","89.48","88.8","89.23","30617089.0"]
 
-	######### Column names
-	names = split(head, ",")[2:end] # Won't need the Date name (fist column) for TimeArray
-	names = ASCIIString[name for name in names]
+    ######### Column names
+    names = split(head, ",")[2:end] # Won't need the Date name (fist column) for TimeArray
+    names = ASCIIString[name for name in names]
 
-	TimeArray(timestamp, vals, names)
+    return TimeArray(timestamp, vals, names)
 end
