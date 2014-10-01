011e1dc47009c2b6ce474514b116360e9064360e
Tony Fong <tony.hf.fong@gmail.com>
2014-10-01 14:00:18 +0700

    remove auth_token from tests

--- test/auth_token.jl
+++
@@ -1,5 +0,0 @@
-facts("auth_token file exists") do
-    context("auth_token is stubbed with message") do
-        @fact length(readdlm(Pkg.dir("Quandl/src/token/auth_token.jl"))) => 28
-    end
-end
