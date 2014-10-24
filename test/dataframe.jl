3592945febf8c3734213d329d5e78ec394be72aa
milktrader <milktrader@gmail.com>
2014-10-24 08:38:37 -0400

    added meaningful tests

--- test/dataframe.jl
+++ test/dataframe.jl
@@ -1,6 +1,20 @@
-facts("timearray works on Request object") do
+df = dataframe(load(Pkg.dir("Quandl/test/response.jld"))["test_response"])
 
-  context("not sure how to include a Request object in test suite") do
-      @fact isempty("") => true
+facts("dataframe works on Request object") do
+
+  context("there are three rows") do
+      @fact  size(df,1) => 3
+  end
+
+  context("there are six columns") do
+      @fact  size(df,2) => 6
+  end
+
+  context("correct value at first row, first column") do
+      @fact  df[1,2] => 103.02
+  end
+
+  context("oldest date first") do
+      @fact df[1,1] < df[3,1] => true
   end
 end
