121f338b7cf807e4796929ba7d99d444f7894e62
milktrader <milktrader@gmail.com>
2015-01-24 21:19:07 -0500

    removing type signature from ss2float method for version 0.4 utilities file

--- src/utilities04.jl
+++ src/utilities04.jl
@@ -6,7 +6,8 @@
 # 100-element Array{Any,1}:
 # SubString{ASCIIString}["102.54","104.14","101.64","104.03","40887168.0"]
 
-function ss2float(ss::Array{Any,1})
+#function ss2float(ss::Array{Any,1})
+function ss2float(ss)
 
     b = zeros(length(ss),length(ss[1]))
     
