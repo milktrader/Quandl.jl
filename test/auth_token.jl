048d38f719a4144146e28615e0fafd3691ba5f5d
brk00 <p.oliveira.castro@gmail.com>
2015-01-30 14:26:24 -0200

    Solved tests' lame bug

--- test/auth_token.jl
+++ test/auth_token.jl
@@ -1,22 +1,22 @@
 facts("Auth token generation is successfull")  do
     include(Pkg.dir("Quandl/src/Quandl.jl"))
 
-    facts("created an empty token file after importing module") do
-        facts("created token file") do
+    context("created an empty token file after importing module") do
+        context("created token file") do
             @fact isfile(Pkg.dir("Quandl/token/auth_token")) => true
         end
 
-        facts("token file is empty") do
+        context("token file is empty") do
             @fact open(readall, Pkg.dir("Quandl/token/auth_token")) => ""
         end
     end
 
-    facts("invalid tokens detected") do
-        facts("detects short token") do
+    context("invalid tokens detected") do
+        context("detects short token") do
             @fact_throws Quandl.set_auth_token("a")
         end
 
-        facts("detects long token") do
+        context("detects long token") do
             @fact_throws Quandl.set_auth_token("a"^21)
         end
     end
