bcebdc202f5a1bc570e4fb36fa7fe26b512ca9a2
milktrader <milktrader@gmail.com>
2015-09-09 13:42:17 -0400

    made meaningful tests again

--- test/timearray.jl
+++ test/timearray.jl
@@ -1,46 +1,36 @@
-## awaiting to to implement JLD-based testing
+using JLD
 
-# upfront apologies for this ... 
-# upfront apologies for this ... 
-facts("awaiting JLD implementation for tests") do
-    context("things will work out fine") do
-        @fact true => 1
-    end
+include(Pkg.dir("Quandl/src/timearray.jl"))
+
+#md  = timearray(load(Pkg.dir("Quandl/test/response.jld"))["missing"])
+taa = timearray(load(Pkg.dir("Quandl/test/response.jld"))["asc"])
+tad = timearray(load(Pkg.dir("Quandl/test/response.jld"))["des"])
+
+facts("timearray works on Request object") do
+
+  context("there are three rows") do
+      @fact  length(taa) => 3
+      @fact  length(tad) => 3
+  end
+
+  context("oldest date first regardless of ordering argument") do
+      @fact  taa[1].timestamp[1] < taa[3].timestamp[1] => true
+      @fact  tad[1].timestamp[1] < tad[3].timestamp[1] => true
+  end
+
+  context("correct value at first row, first column, regardless of ordering argument") do
+      @fact  taa[1][1].values[1] => 28.75
+      @fact  tad[1][1].values[1] => 112.49
+  end
 end
+ 
+facts("floats and NaNs present") do
 
-# using TimeSeries, HDF5, JLD, Requests
-# 
-# include(Pkg.dir("Quandl/src/timearray.jl"))
-# 
-# md  = timearray(load(Pkg.dir("Quandl/test/response.jld"))["missing"])
-# taa = timearray(load(Pkg.dir("Quandl/test/response.jld"))["asc"])
-# tad = timearray(load(Pkg.dir("Quandl/test/response.jld"))["des"])
-# 
-# facts("timearray works on Request object") do
-# 
-#   context("there are three rows") do
-#       @fact  length(taa) => 3
-#       @fact  length(tad) => 3
-#   end
-# 
-#   context("oldest date first regardless of ordering argument") do
-#       @fact  taa[1].timestamp[1] < taa[3].timestamp[1] => true
-#       @fact  tad[1].timestamp[1] < tad[3].timestamp[1] => true
-#   end
-# 
-#   context("correct value at first row, first column, regardless of ordering argument") do
-#       @fact  taa[1][1].values[1] => 103.02
-#       @fact  tad[1][1].values[1] => 103.02
-#   end
-# end
-#  
-# facts("floats and NaNs present") do
-# 
-#   context("NaN fills in missing values slot") do
-#       @fact  isnan(sum(md.values[:,1])) => true
-#   end
-# 
-#   context("existing values remain floats") do
-#       @fact  sum(md.values[:,2]) => 66.86
-#   end
-# end
+  context("NaN fills in missing values slot") do
+#      @pending isnan(sum(md.values[:,1])) => true
+  end
+
+  context("existing values remain floats") do
+#      @fact  sum(md.values[:,2]) => 66.86
+  end
+end
