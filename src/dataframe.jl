5723d8191ddf64b6bf60c52d8686597e27ec9d5e
John M Kuhn <jmkuhn@starpower.net>
2017-09-28 16:11:31 -0400

    Replace readtable() with CSV.read()

--- src/dataframe.jl
+++ src/dataframe.jl
@@ -1,21 +1,6 @@
 function dataframe(resp::Requests.Response)
 
-    buffer = PipeBuffer() # open a buffer in which to dump data
-    df     = DataFrame()  # init empty DataFrame
-
-    try
-        # Write the data to the buffer
-        write(buffer, Requests.text(resp))
-
-        # Use DataFrame's readtable to read the data directly from buffer
-        df = readtable(buffer)
-        
-        # Convert dates to Dates object
-        df[:Date] = Date[Date(d) for d in df[:Date]]
-
-    finally
-        close(buffer)   
-    end
+    df = CSV.read(IOBuffer(Requests.text(resp)))
 
     # force oldest date to first row
 
